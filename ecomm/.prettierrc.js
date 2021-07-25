102046792c6bf99b0745b57e53f3bb5cadedffd7
Barış Selayet <bselayet@maritimetrainer.com>
2021-07-25 17:23:59 +0300

    added ecomm app

---
+++ ecomm/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 90,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  insertPragma: false
+};
---
+++ ecomm/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['fsghsjulous1321ew1f3w123f13w1e31fw']
+  })
+);
+app.use(authRouter);
+app.use(productsRouter);
+
+app.listen(3000, () => {
+  console.log('listening');
+});
