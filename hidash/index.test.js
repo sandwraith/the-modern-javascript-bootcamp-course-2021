7bf46b832e52390278136b4af0fe98fc606778f4
Barış Selayet <bselayet@maritimetrainer.com>
2021-07-26 21:30:42 +0300

    added test section codes

---
+++ hidash/index.test.js
@@ -0,0 +1,36 @@
+const assert = require("assert");
+const { forEach, map } = require("./index");
+
+const test = (desc, fn) => {
+  console.log("----", desc);
+  try {
+    fn();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+test("test forEach Function", () => {
+  let sum = 0;
+  forEach([1, 2, 3], (value) => {
+    sum += value;
+  });
+
+  assert.strictEqual(sum, 6, "expected foreach to sum the array");
+
+  // if (sum !== 6) {
+  //   throw new Error(`Expected summing array to equal 6`);
+  // }
+});
+
+test("The map function", () => {
+  const result = map([1, 2, 3], (value) => {
+    return value * 2;
+  });
+
+  assert.deepStrictEqual(result, [2, 4, 6]);
+
+  // assert.strictEqual(result[0], 2);
+  // assert.strictEqual(result[1], 4);
+  // assert.strictEqual(result[2], 6);
+});
