7f70f87fd0496cb3434ec640ac0ff7b8c01ff2fd
Barış Selayet <bselayet@maritimetrainer.com>
2021-07-27 12:05:55 +0300

    updated testing code

--- hidash/index.test.js
+++ hidash/index.test.js
@@ -1,36 +1,36 @@
-const assert = require("assert");
-const { forEach, map } = require("./index");
+const assert = require('assert');
+const { forEach, map } = require('./index');
 
-const test = (desc, fn) => {
-  console.log("----", desc);
-  try {
-    fn();
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+// const test = (desc, fn) => {
+//   console.log("----", desc);
+//   try {
+//     fn();
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
 
-test("test forEach Function", () => {
-  let sum = 0;
-  forEach([1, 2, 3], (value) => {
-    sum += value;
-  });
+it('test forEach Function', () => {
+	let sum = 0;
+	forEach([ 1, 2, 3 ], (value) => {
+		sum += value;
+	});
 
-  assert.strictEqual(sum, 6, "expected foreach to sum the array");
+	assert.strictEqual(sum, 6, 'expected foreach to sum the array');
 
-  // if (sum !== 6) {
-  //   throw new Error(`Expected summing array to equal 6`);
-  // }
+	// if (sum !== 6) {
+	//   throw new Error(`Expected summing array to equal 6`);
+	// }
 });
 
-test("The map function", () => {
-  const result = map([1, 2, 3], (value) => {
-    return value * 2;
-  });
+it('The map function', () => {
+	const result = map([ 1, 2, 3 ], (value) => {
+		return value * 2;
+	});
 
-  assert.deepStrictEqual(result, [2, 4, 6]);
+	assert.deepStrictEqual(result, [ 2, 4, 6 ]);
 
-  // assert.strictEqual(result[0], 2);
-  // assert.strictEqual(result[1], 4);
-  // assert.strictEqual(result[2], 6);
+	// assert.strictEqual(result[0], 2);
+	// assert.strictEqual(result[1], 4);
+	// assert.strictEqual(result[2], 6);
 });
